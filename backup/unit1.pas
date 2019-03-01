1e4c2b33b8ac5e2473fc71f0ff4f2a772efaea05
Necta177 <f.iss@ludus-academie.com>
2019-03-01 09:55:26 +0100

    TP convertisseur monnaie v1.0

---
+++ backup/unit1.pas
@@ -0,0 +1,52 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, SpinEx, Forms, Controls, Graphics, Dialogs,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    convertir: TButton;
+    convertir2: TButton;
+    euroo: TSpinEditEx;
+    doollar: TSpinEditEx;
+    euro: TLabel;
+    dollar: TLabel;
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure convertir2Click(Sender: TObject);
+    procedure convertirClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.convertirClick(Sender: TObject);
+begin
+    doollar.Value:=euroo.Value*1.14;
+end;
+
+procedure TForm1.convertir2Click(Sender: TObject);
+begin
+   euroo.value:=doollar.value/1.14;
+end;
+
+end.
+
